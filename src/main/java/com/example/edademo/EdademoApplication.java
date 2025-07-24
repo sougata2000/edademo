648c57854c2b0cbea11adac81955bde7c70a6457
sougata2000 <bagchi.sougata@gmail.com>
2025-07-24 11:03:36 +0530

    initial commit

---
+++ src/main/java/com/example/edademo/EdademoApplication.java
@@ -0,0 +1,14 @@
+package com.example.edademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdademoApplication.class, args);
+		System.out.println("i = " );
+	}
+
+}
